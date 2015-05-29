903393badc6d8cffb01c8ca763191fa364e566f1
Samar Panda <samar.panda84@gmail.com>
2015-05-29 07:08:23 +0530

    Using props in React

---
+++ excercises/2-props/app.js
@@ -0,0 +1,52 @@
+var React = require('react');
+var md5 = require('MD5');
+var warning = require('react/lib/warning');
+var validateEmail = require('./validateEmail');
+
+var GRAVATAR_URL = "http://gravatar.com/avatar";
+var USERS = [
+	{ id: 1, name: "Rayn Florence", email: 'rpflorence@gmail.com' },
+	{ id: 2, name: "Mickel Jackson", email: 'mjijackson@gmail.com' },
+	{ id: 3, name: "Samar Panda", email: 'samar.panda84@gmail.com' }
+];
+
+var emailType = (props, propName, componentName) => {
+	warning(
+		validateEmail(props.email),
+		`Invalid email '${props.email}' sent to 'Gravatar'. Check the render method of '${componentName}'.`
+	);
+};
+
+var Gravatar = React.createClass({
+	propTypes: {
+		user: React.PropTypes.shape({
+			email: emailType,
+			name: React.PropTypes.string.isRequired,
+			id: React.PropTypes.number.isRequired
+		}).isRequired
+	},
+	render() {
+		var { user, size } = this.props;
+		var hash = md5(user.email);
+		var url = `${GRAVATAR_URL}/${hash}?s=${size*2}`;
+		return <img src={url} width={size} />
+	}
+});
+
+var App = React.createClass({
+	render() {
+		var users = USERS.map((user) => {
+			return (
+				<li key={user.id}><Gravatar user={user} size={36} />{user.name}</li>
+			);
+		});
+		return (
+		<div>
+			<h1>Users</h1>
+			<ul>{users}</ul>
+		</div>
+		);
+	}
+});
+
+React.render(<App/>, document.body);
